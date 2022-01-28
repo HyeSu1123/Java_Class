2bf383c675bce5d1d2f34927520cca5a0659b12c
hyesu <toffofl@gmail.com>
2022-01-28 14:43:18 +0900

    backup

---
+++ day13to19/src/koreait/review/Shape.java
@@ -0,0 +1,38 @@
+package koreait.review;
+
+public class Shape {	//상위클래스 -> 상속받은 클래스는 Triangle, Square
+
+	static final int MAX_WIDTH=100;	//변하지 않는 값.(상수) 모든 객체가 같은 값 사용
+	static final int MAX_HEIGHT = 200;
+	
+	private String shapeName;	//도형이름
+	private int width;;			//도형의 크기 중 너비
+	private int height;			//도형의 크기 중 높이
+	String etc = "연습용";			//기본 접근자 (패키지같은 클래스에 사용할 수 있다.
+	protected int count = 12;	//자식 클래스이면 어느 패키지 이던지 사용할 수 있다
+	
+	
+	//필드 초기화 생성자 : 필드값은 생성자로만 초기화한다.(예시 : 요구 조건은 초기화된 필드값은 변경 하지 않는다)
+	public Shape(String shapeName, int width, int height) {
+		this.shapeName = shapeName;
+		this.width = width;
+		this.height = height;
+	}
+	
+	//getter 메소드만 허용 (setter 는 허용 안함(요구 조건은 초기화된 필드값은 변경 하지 않는다))
+	public int getHeight() {
+		return height;
+	}
+	public String getShapeName() {
+		return shapeName;
+	}
+	public int getWidth() {
+		return width;
+	}
+	
+	//넓이를 구해서 리턴해주는 메소드 : getArea
+	public int getArea() {
+		return width * height;
+	}
+	//기본 생성자를 사용할 수 없는 클래스
+}
---
+++ day13to19/src/koreait/review/Square.java
@@ -0,0 +1,35 @@
+package koreait.review;
+
+public class Square extends Shape{
+
+
+	protected String color; 	//자식 클래스 Sqare만 갖는 필드
+								//Square 클래스가 부모, 그 자식 클래스들은 다른 패키지에서도 사용할 수 있는 필드
+	
+//	public Square(String color) {
+//		//super() 자동 호출 -> 오류
+//		this.color = color;
+//	}
+
+	public Square(String shapeName, int width, int height, String color) {
+		super(shapeName, width, height);
+		this.color = color;
+	}
+	
+	public Square(String color) { //자식 클래스는 상속 필드, 상속 메소드 외에 다른 필드와 메소드를 정의할 수 있다
+		super("정사각형",10,10);
+		this.color = color;
+	}
+	
+	public void printWidth() {
+		System.out.println("정사각형 너비 = "+ getWidth()); //부모 클래스의 필드와 메소드는 자식 클래스가 바로 접근할 수 있다(super 키워드 생략)
+	}
+	public void printEtc() {
+		System.out.println("기타정보 : "+etc); //부모 클래스의 필드와 메소드는 자식 클래스가 바로 접근할 수 있다(super 키워드 생략)
+	}
+	
+	public int getArea() {	//오버라이드 메소드(재정의)
+		return getWidth()*getWidth();
+	}
+	
+}
